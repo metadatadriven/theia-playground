98d3a37b0e0b1a4e748915f56249f434e6ebf135
Stuart Malcolm <stuart@metadatadriven.com>
2023-11-22 15:17:44 +0000

    thei built from yo generator with empty extension

---
+++ empty-extension/src/browser/empty-extension-contribution.ts
@@ -0,0 +1,7 @@
+import { injectable } from '@theia/core/shared/inversify';
+
+@injectable()
+// Add contribution interface to be implemented, e.g. "EmptyExtensionContribution implements CommandContribution"
+export class EmptyExtensionContribution{
+
+}
